c609df18ba308f309b5e8a9c8da4026ad1b37bc3
KaranJoshi97 <josh0024@algonquinlive.com>
2018-09-27 19:30:17 -0400

    Finished lab 3

--- app/src/main/java/AndroidLabs/ListItemsActivity.java
+++ app/src/main/java/AndroidLabs/ListItemsActivity.java
@@ -1,13 +1,139 @@
 package AndroidLabs;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ListItemsActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    // Step 7 for Lab 3
+    ImageButton imageButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Log.i(ACTIVITY_NAME, "In onCreate()"); // Step 3 for Lab 3
+
+        imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                        }
+                    }
+        });
+
+        // Step 8 for Lab 3
+        Switch switch1 = (Switch) findViewById(R.id.switch1);
+
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compButton, boolean bool) {
+                CharSequence text;
+                int duration;
+                Toast toast;
+                if (compButton.isChecked()) {
+                    // "Switch is On";
+                    duration = Toast.LENGTH_SHORT;
+                    toast = Toast.makeText(getApplicationContext(), R.string.onSwitch, duration);
+                } else {
+                    // "Switch is Off";
+                    duration = Toast.LENGTH_LONG;
+                    toast = Toast.makeText(getApplicationContext(), R.string.offSwitch, duration);
+                }
+                toast.show(); // display message box
+            }
+        });
+
+        // Step 9 for Lab 3
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compButton, boolean bool) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                // Step 10 for Lab 3
+                builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+                        .setTitle(R.string.dialog_title)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent resultIntent = new Intent();
+                                resultIntent.putExtra("Response", "Here is my response");
+                                setResult(Activity.RESULT_OK, resultIntent);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+            }
+        });
+
+
     }
+
+    // Step 7 for Lab 3
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageButton1.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
+
 }
--- app/src/main/java/AndroidLabs/StartActivity.java
+++ app/src/main/java/AndroidLabs/StartActivity.java
@@ -1,13 +1,78 @@
 package AndroidLabs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Log.i(ACTIVITY_NAME, "In onCreate()"); // Step 3 for Lab 3
+
+        // Step 6 for Lab 3
+        Button mainButton = (Button) findViewById(R.id.button);
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextScreen = new Intent(StartActivity.this, ListItemsActivity.class);
+                startActivityForResult(nextScreen, 50);
+            }
+        });
+
     }
+    // Step 6 for Lab 3
+    @Override
+    public void onActivityResult(int requestCode, int responseCode, Intent data){
+
+        // Step 11 for Lab 3
+        if(requestCode == 50 && responseCode == Activity.RESULT_OK) {
+            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+            String messagePassed = data.getStringExtra("Response");
+            Toast toast = Toast.makeText(this, messagePassed, Toast.LENGTH_LONG );
+            toast.show();
+        }
+    }
+
+    // Steps 2 and 3 for Lab 3
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
 }
