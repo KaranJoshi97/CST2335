d3fe63f8dec8bf90a74f8f0297fdc82b8e85c7d6
KaranJoshi97 <josh0024@algonquinlive.com>
2018-11-08 13:20:26 -0500

    Finished Lab 6

---
+++ app/src/main/java/AndroidLabs/HttpUtils.java
@@ -0,0 +1,35 @@
+package AndroidLabs;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HttpUtils {
+    public static Bitmap getImage(URL url){
+        HttpURLConnection connection = null;
+        try{
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            return BitmapFactory.decodeStream(connection.getInputStream());
+        }catch(Exception e){
+            return null;
+        }finally{
+            if(connection != null){
+                connection.disconnect();
+            }
+        }
+    }
+
+    public static Bitmap getImage(String urlString){
+        try{
+            URL url = new URL(urlString);
+            return getImage(url);
+        }catch(MalformedURLException e){
+            return null;
+        }
+    }
+
+}
